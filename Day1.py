99706bd4662291d17f0982bce76886e27d3f3580
Keshab Raj Adhikari <keshabraja3@gmail.com>
2026-04-27 08:29:50 +0545

    day1 python foundation

---
+++ Day1.py
@@ -0,0 +1,33 @@
+#python as a command
+
+print("Hello")
+
+name= "keshab"
+print(name)
+
+print("Hello, Everyone! We are learning python")
+
+
+##python as a calculator
+#for math, we don't use quotesm python just does the work
+
+print("what is 5+5")
+print(100-25)
+
+# user input taking input from user
+user_name= input("Type your name and press Enter: ")
+print(user_name)
+
+a="hello world"
+print(a + " hi")
+
+# multyplying text
+
+print("hi " * 10)
+
+#mini game
+
+print ("\n---Quick Game ----")
+color= input("give me a color: ")
+animal= input("give me an animal: ")
+print("Look! A " + color + " " + animal + " is dancing in the classroom!")
